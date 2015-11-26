7541947dd70399805acc2f07cda0b5a2dc9ed22f
sohanvichare <sohanvichare@Sohans-MacBook-Pro.local>
2015-11-26 13:52:09 -0800

    Initial Commit
    Initial Commit

---
+++ script.js
@@ -0,0 +1,38 @@
+$( document ).ready(function(){
+  $('.button-collapse').sideNav({
+      menuWidth: 300, // Default is 240
+      edge: 'left', // Choose the horizontal origin
+    }
+  );
+
+  $('.med').click(function(){
+    window.open("https://www.medium.com/@sohanvichare", '_blank')
+  });
+
+  $('.tw').click(function(){
+    window.open("https://www.twitter.com/@sohanvichare", '_blank')
+  });
+
+  $('.in').click(function(){
+    window.open("https://www.linkedin.com/in/sohanvichare", '_blank')
+  });
+
+  $('.fb').click(function(){
+    window.open("https://www.facebook.com/sohanvichare26", '_blank')
+  });
+
+  $(function(){
+      $(".element").typed({
+        strings: ["California Young Arts Scholar.", "iOS developer.", "hacker.", "artist.", "podcast host.", "robotics team captain.", "web designer.", "high school student.", "thespian.", "avid blogger.", "iOS designer.", "machine-learning enthusiast.", "award-winning debater.", "theater geek.", "award-winning speaker.", "team player."],
+        typeSpeed: 0,
+        loop: true
+      });
+  });
+
+   $('.scrollspy').scrollSpy();
+
+   $('.collapsible').collapsible({
+      accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+    });
+
+});
