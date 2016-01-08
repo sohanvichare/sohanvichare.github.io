b9528c8ef0ea253a35f403beabe5353e6cd25dc8
sohanvichare <sohanvichare@Sohans-MacBook-Pro.local>
2016-01-07 20:47:29 -0800

    add github link

--- script.js
+++ script.js
@@ -21,6 +21,10 @@ $( document ).ready(function(){
     window.open("https://www.facebook.com/sohanvichare26", '_blank')
   });
 
+  $('.git').click(function(){
+    window.open("http://github.com/sohanvichare", '_blank')
+  });
+
   $(function(){
       $(".element").typed({
         strings: ["a California Arts Scholar.", "an iOS developer.", "a drone enthusiast.", "an artist.", "a robotics team captain.", "a web designer.", "a high school student.", "a thespian.", "an avid blogger.", "an iOS designer.", "a machine-learning enthusiast.", "an award-winning debater.", "a theater geek.", "an award-winning speaker.", "a team player."],
