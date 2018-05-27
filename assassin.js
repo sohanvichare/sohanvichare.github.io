99a3b56ead55f53c04116a65e2be1936b801caed
Sohan Vichare <sovicx@gmail.com>
2018-05-27 00:43:45 -0700

    keren edit

--- assassin.js
+++ assassin.js
@@ -1,6 +1,21 @@
 
 $(function() {
 
+	name = ""
+
+	var config = {
+		apiKey: "AIzaSyB7OmimUnmFeBmbqBfep19INkSUFOhur60",
+		authDomain: "assassin-2109b.firebaseapp.com",
+		databaseURL: "https://assassin-2109b.firebaseio.com",
+		projectId: "assassin-2109b",
+		storageBucket: "assassin-2109b.appspot.com",
+		messagingSenderId: "525676260292"
+	};
+	firebase.initializeApp(config);
+
+	var database = firebase.database();
+
+
 	$(".info").hide()
 
 	$( "#lockIn").click(function() {
@@ -19,6 +34,12 @@ $(function() {
 				$(".info").show("slow")
 				$("#nameLi").text("Name: " + name)
 				$("#objectLi").text("Object: " + object)
+
+
+				firebase.database().ref('users/' + name.replace(/\s/g, '')).set({
+			    name: name,
+			   	object: object,
+			  });
 
 			}
 
